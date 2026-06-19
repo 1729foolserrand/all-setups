7f8c2b03acba812eb568489594573aed723f497f
ganesh naik noonsavath <96568758+1729foolserrand@users.noreply.github.com>
2026-06-19 05:45:39 +0530

    Upgrade Nexus to version 3.93.0-06 and Java to 21
    Updated Nexus installation script to use version 3.93.0-06 and Java 21.

--- nexus.sh
+++ nexus.sh
@@ -1,33 +1,12 @@
 sudo yum update -y
 sudo yum install wget -y
-sudo yum install java-17-amazon-corretto-jmods -y
+sudo yum install java-21-amazon-corretto-jmods -y
 sudo mkdir /app && cd /app
-sudo wget https://download.sonatype.com/nexus/3/nexus-3.79.1-04-linux-x86_64.tar.gz
-sudo tar -xvf nexus-3.79.1-04-linux-x86_64.tar.gz
-sudo mv nexus-3.79.1-04 nexus
+sudo wget https://download.sonatype.com/nexus/3/nexus-3.93.0-06-linux-x86_64.tar.gz
+sudo tar -xvf nexus-3.93.0-06-linux-x86_64.tar.gz
 sudo adduser nexus
-sudo chown -R nexus:nexus /app/nexus
-sudo chown -R nexus:nexus /app/sonatype*
-sudo sed -i '27  run_as_user="nexus"' /app/nexus/bin/nexus
-sudo tee /etc/systemd/system/nexus.service > /dev/null << EOL
-[Unit]
-Description=nexus service
-After=network.target
-
-[Service]
-Type=forking
-LimitNOFILE=65536
-User=nexus
-Group=nexus
-ExecStart=/app/nexus/bin/nexus start
-ExecStop=/app/nexus/bin/nexus stop
-User=nexus
-Restart=on-abort
-
-[Install]
-WantedBy=multi-user.target
-EOL
-sudo chkconfig nexus on
-sudo systemctl start nexus
-sudo systemctl enable nexus
-sudo systemctl status nexus
+sudo chown -R nexus:nexus /app/nexus-3.93.0-06
+sudo chown -R nexus:nexus /app/sonatype-work
+sudo su - nexus
+sudo cd /app//nexus-3.93.0-06/bin/
+sudo ./nexus start
